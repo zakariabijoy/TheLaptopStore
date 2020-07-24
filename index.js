351df5998d442996ee1379157a33ceb9fab82b6b
zakariabijoy <zakaria@vonome.com>
2020-07-25 03:01:00 +0600

    the laptop store project part 2

--- index.js
+++ index.js
@@ -9,14 +9,41 @@ const server = http.createServer((req, res) =>{
     const pathName = url.parse(req.url, true).pathname;
     const id = url.parse(req.url, true).query.id;
 
+    // products overView
     if(pathName === '/products' || pathName === '/'){
         res.writeHead(200, { 'content-type': 'text/html' });
-        res.end('this is the products page!');
 
+        fs.readFile(`${__dirname}/templates/template-overview.html`, 'utf-8', (err, data) => {
+           let overviewOutput = data;
+
+            fs.readFile(`${__dirname}/templates/template-card.html`, 'utf-8', (err, data) => {
+                
+                const cardsOutput = laptopData.map(el => replaceTemplate(data, el)).join('');
+                overviewOutput = overviewOutput.replace('{%Cards%}', cardsOutput);
+
+                res.end(overviewOutput);
+            });
+        });
+        
+    // laptop details
     }else if(pathName === '/laptop' && id < laptopData.length){
         res.writeHead(200, { 'content-type': 'text/html' });
-        res.end(`this is the laptop page for ${id}!`);
-    }else{
+
+        fs.readFile(`${__dirname}/templates/template-laptop.html`, 'utf-8', (err, data) =>{
+            const laptop = laptopData[id];               
+            const output = replaceTemplate(data, laptop);
+            res.end(output);
+        });
+    
+    }else if((/\.(jpg|jpeg|png|gif|)$/i).test(pathName)){
+        fs.readFile(`${__dirname}/data/img${pathName}`,(err, data) =>{
+            res.writeHead(200, { 'Content-type': 'image/jpg'});
+            res.end(data);
+        })
+    }
+    
+    // url not found
+    else{
         res.writeHead(400, { 'content-type': 'text/html' });
         res.end('bad request!');
     }
@@ -26,3 +53,17 @@ const server = http.createServer((req, res) =>{
 server.listen(1337, '127.0.0.1', () =>{
     console.log('listening for requests now');
 })
+
+function replaceTemplate(originaHtml, laptop) {
+    let output = originaHtml.replace(/{%ProductName%}/g, laptop.productName);
+    output = output.replace(/{%Image%}/g, laptop.image);
+    output = output.replace(/{%Price%}/g, laptop.price);
+    output = output.replace(/{%Screen%}/g, laptop.screen);
+    output = output.replace(/{%Ram%}/g, laptop.ram);
+    output = output.replace(/{%Cpu%}/g, laptop.cpu);
+    output = output.replace(/{%Storage%}/g, laptop.storage);
+    output = output.replace(/{%Description%}/g, laptop.description);
+    output = output.replace(/{%Id%}/g, laptop.id); 
+
+    return output;
+}
